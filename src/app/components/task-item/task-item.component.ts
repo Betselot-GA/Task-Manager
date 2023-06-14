be56c2446eef5a309619e557114a389d2b215e32
Betselot-GA <betselotgetnet2227@gmail.com>
2023-06-14 10:50:47 -0400

    started angular task manager project

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task: Task = {
+    id: 0,
+    text: '',
+    day: '',
+    reminder: false,
+  }
+
+  constructor(){}
+  ngOnInit(): void {
+  }
+
+}
